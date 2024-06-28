c8e7e38c6270d6dc5690c2f41f6e3a06eddc475e
kartik kaushik <kartikgeu@gmail.com>
2024-06-28 16:36:56 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,112 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+const Faq = () => {
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
+
+  const toggleAccordion = (faqNumber) => {
+    switch (faqNumber) {
+      case 1:
+        setIsOpen1(!isOpen1);
+        setIsOpen2(false);
+        setIsOpen3(false);
+        break;
+      case 2:
+        setIsOpen1(false);
+        setIsOpen2(!isOpen2);
+        setIsOpen3(false);
+        break;
+      case 3:
+        setIsOpen1(false);
+        setIsOpen2(false);
+        setIsOpen3(!isOpen3);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <div className="w-full max-w-screen-xl mx-auto flex flex-col items-center">
+      <div className="relative w-full">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          width={1350}
+          height={170}
+          src="/rectangle.jpg"
+          alt="rectangular background"
+          className="object-cover w-full h-56 md:h-100"
         />
       </div>
+      <div className="absolute block w-160 h-96 px-20 pb-10 rounded-xl bg-white text-center mt-24 shadow-lg">
+       <div className="relative">
+      <span className="absolute left-1/2 transform -translate-x-28 text-purple-950 text-3xl font-bold pt-8 sm:pt-10  -translate-x-18">FAQs</span>
+    </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    <div className="relative mt-8 md:mt-10">
+      <Image
+        src="/star.svg"
+        alt="star svg image"
+        width={100}
+        height={100}
+        className="w-10 ml-5 text-purple-900"
+      />
+    </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        {/* FAQ Section 1 */}
+        <div className="mb-4 md:mb-6">
+          <p className="font-bold text-lg md:text-xl text-purple-900">
+            What is Frontend Mentor and how does it help me?{' '}
+            <button onClick={() => toggleAccordion(1)} className={`text-white rounded-full w-7 h-7 md:w-10 md:h-10 ${isOpen1 ? 'bg-neutral-950' : 'bg-purple-950'}`}>
+              {isOpen1 ? '-' : '+'}
+            </button>
           </p>
-        </a>
+          
+          {isOpen1 && (
+            <p className="text-purple-900 mt-2 md:text-base">
+              Frontend Mentor offers invaluable real-world <br className="hidden md:inline" /> web development challenges, sharpening
+              my <br className="hidden md:inline" /> HTML, CSS, and JavaScript skills.
+            </p>
+          )}
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+        {/* FAQ Section 2 */}
+        <div className="mb-4 md:mb-6">
+          <p className="font-bold text-lg md:text-xl text-purple-900">
+            Is Frontend Mentor Free?{' '}
+            <button onClick={() => toggleAccordion(2)} className={`text-white rounded-full w-7 h-7 md:w-10 md:h-10 ${isOpen2 ? 'bg-neutral-950' : 'bg-purple-950'}`}>
+              {isOpen2 ? '-' : '+'}
+            </button>
           </p>
-        </a>
+          
+          {isOpen2 && (
+            <p className="text-purple-900 mt-2 md:text-base">
+              Yes, it is free and it's helpful.
+            </p>
+          )}
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        {/* FAQ Section 3 */}
+        <div>
+          <p className="font-bold text-lg md:text-xl text-purple-900">
+            Can I use Frontend Mentor projects in building my portfolio?{' '}
+            <button onClick={() => toggleAccordion(3)} className={`text-white rounded-full w-7 h-7 md:w-10 md:h-10 ${isOpen3 ? 'bg-neutral-950' : 'bg-purple-950'}`}>
+              {isOpen3 ? '-' : '+'}
+            </button>
           </p>
-        </a>
+          
+          {isOpen3 && (
+            <p className="text-purple-900 mt-2 md:text-base">
+              Yes, you can use it in building your portfolio.
+            </p>
+          )}
+        </div>
       </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Faq;
